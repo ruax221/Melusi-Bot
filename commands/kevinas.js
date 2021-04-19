6545a446c56fb3c6a47c0059bc60ba247cbe8d81
ruax221 <edvisss222@gmail.com>
2021-04-19 19:24:45 +0300

    melusi-bot

---
+++ commands/kevinas.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'kevinas',
+    description: "cia atsiuncia debilo info",
+    execute(message, args){
+        message.channel.send('Kevinas Karbauskas IP:62.80.225.76 55.961916 23.282901 Address: Šiauliu Rajonas Ievos g. 9, 76223 Vijoliai');
+
+
+
+
+    }
+}
---
+++ commands/youtube.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'youtube',
+    description: "sends the youtbe link",
+    execute(message, args){
+        message.channel.send('https://www.youtube.com/channel/UCI_9WUZuIeZXgYfXtparWNQ');
+
+
+
+
+    }
+}
